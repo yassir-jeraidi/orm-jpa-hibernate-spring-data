2ae2f8f3a4fe8e2e72a2feb9600f2512e046661b
YassirJr <yassir.junior.03@gmail.com>
2025-03-12 23:53:18 +0000

    Initial commit

---
+++ src/test/java/com/orm/spring_data/OrmJpaHibernateSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orm.spring_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmJpaHibernateSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
